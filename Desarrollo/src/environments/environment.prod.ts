22422d9d0be0ac4d1e168aab67247de7ed27b57c
AndersonJimenez <asjimenez@asesoftware.com>
2018-12-10 16:52:07 -0500

    Se agrega linea base de desarrollo

---
+++ Desarrollo/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  ambiente: 'produccion',
+  ip: 'http://10.23.18.242:'
+};
---
+++ Desarrollo/src/environments/environment.qa.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    ambiente: 'calidad',
+    ip: 'http://10.23.18.241:'
+};

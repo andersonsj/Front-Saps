8ef0967b200892fc80539acd3bfbf4f4a3e07b5b
AndersonJimenez <asjimenez@asesoftware.com>
2018-12-19 10:08:00 -0500

    Se ajustan problemas reportados en escaner de lint

--- src/app/component/producto/cotizacion-producto/cotizacion-producto.component.ts
+++ src/app/component/producto/cotizacion-producto/cotizacion-producto.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { Producto } from 'src/app/core/model/producto';
+import { Producto } from 'src/app/core/models/producto';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DataService } from 'src/app/core/services/data.service';
 import { ProductoService } from '../producto.service';
@@ -32,7 +32,7 @@ export class CotizacionProductoComponent implements OnInit {
   private cantidadProducto: any;
   private numero = [];
   private producto: Array<any>;
-  private habilita: boolean = false;
+  private habilita = false;
   private sw: boolean;
 
 
@@ -51,10 +51,6 @@ export class CotizacionProductoComponent implements OnInit {
   ngOnInit() {
     console.log(this.valor);
 
-  }
-
-  holaMundo(){
-    console.log("Hola Hola Hola");
   }
 
   restarCantidad(cont: number) {
@@ -82,68 +78,27 @@ export class CotizacionProductoComponent implements OnInit {
         precioUnidad: null,
         precioTotal: null,
         fichaTecnica: null
-      }; 
+      };
     }
   }
 
-  sumarCantidad(cont: number) {  
-    console.log("cont sumar: "+cont);
+  sumarCantidad(cont: number) {
     this.infoProducto = this.numero[cont];
     this.cantidadProducto = this.infoProducto.cantidad;
     this.cantidadProducto = this.cantidadProducto + 1;
     this.infoProducto.cantidad = this.cantidadProducto;
     this.infoProducto.precioTotal = this.infoProducto.precioUnidad * this.infoProducto.cantidad;
-      console.log(this.infoProducto);
-      this.data.guardarLocal('precioTotal', 'this.infoProducto.precioTotal');
-      this.numero[cont] =  this.infoProducto;
-      this.precioTotal = 0;
-      console.log("Este es el precio total previo: "+this.precioTotal);
-      for (let j = 0; j < this.numero.length; j++) {
-        this.precioTotal = this.numero[j].precioTotal + this.precioTotal;
-        console.log('entre');
-      }
-      console.log("Este es el precio total despues: "+this.precioTotal);
-      this.valorTotalCot.emit(this.precioTotal);
-      this.precioPagarCot.emit(this.precioTotal);
-      this.infoProducto={
-        sku: null,
-        nombre: null,
-        cantidad: null,
-        precioUnidad: null,
-        precioTotal: null,
-        fichaTecnica: null
-      };
-  }
-
-  listaProductos() {
-    console.log("length1");
-    console.log(this.numero.length);
-    console.log(this.infoProducto)
-
-    let i: any;
-    i = this.numero.length;
-    console.log("valor i" + i);
-    if (i > 0){
-      console.log(this.numero[i-1]);
-    }
-    this.numero[i] = this.infoProducto;
-
-    console.log(this.numero);
+    console.log(this.infoProducto);
+    this.data.guardarLocal('precioTotal', 'this.infoProducto.precioTotal');
+    this.numero[cont] = this.infoProducto;
     this.precioTotal = 0;
-    console.log("Este es el precio total previo: "+this.precioTotal);
     for (let j = 0; j < this.numero.length; j++) {
       this.precioTotal = this.numero[j].precioTotal + this.precioTotal;
       console.log('entre');
     }
-    console.log('el precio est aqui');
-    console.log(this.precioTotal);
     this.valorTotalCot.emit(this.precioTotal);
     this.precioPagarCot.emit(this.precioTotal);
-    
-    console.log("length2");
-    console.log(this.numero.length);
-
-    this.infoProducto={
+    this.infoProducto = {
       sku: null,
       nombre: null,
       cantidad: null,
@@ -151,75 +106,89 @@ export class CotizacionProductoComponent implements OnInit {
       precioTotal: null,
       fichaTecnica: null
     };
-    
+  }
+
+  listaProductos() {
+
+    let i: any;
+    i = this.numero.length;
+    if (i > 0) {
+      console.log(this.numero[i - 1]);
+    }
+    this.numero[i] = this.infoProducto;
+
+    this.precioTotal = 0;
+    for (let j = 0; j < this.numero.length; j++) {
+      this.precioTotal = this.numero[j].precioTotal + this.precioTotal;
+    }
+
+    this.valorTotalCot.emit(this.precioTotal);
+    this.precioPagarCot.emit(this.precioTotal);
+
+    this.infoProducto = {
+      sku: null,
+      nombre: null,
+      cantidad: null,
+      precioUnidad: null,
+      precioTotal: null,
+      fichaTecnica: null
+    };
+
     this.habilita = true;
     console.log(this.habilita);
-  
-    (<HTMLInputElement>document.getElementById("txtBuscarSku")).value = '';
+
+    (<HTMLInputElement>document.getElementById('txtBuscarSku')).value = '';
   }
 
   buscarProducto(buscarSku: any) {
-    console.log("lololololo");
-    if(buscarSku == '' || isNaN(Number(buscarSku))){
-      alert("El SKU debe ser númerico");
-      (<HTMLInputElement>document.getElementById("txtBuscarSku")).value = '';
-    }
-    else{
+
+    if (buscarSku === '' || isNaN(Number(buscarSku))) {
+      alert('El SKU debe ser númerico');
+      (<HTMLInputElement>document.getElementById('txtBuscarSku')).value = '';
+    } else {
       this.sw = false;
       this.productoService.getBuscarProductos(buscarSku).subscribe(
         (data) => {
           this.datos = data.value;
-          console.log("length data.value");
-          console.log(data.value);
-          
-          if (data.value.length == 1){
+          if (data.value.length === 1) {
             this.sw = true;
             this.infoProducto.sku = this.datos[0].sku;
-            console.log("sku datos-infoproducto");
-            console.log(this.infoProducto.sku);
             this.infoProducto.nombre = this.datos[0].nombre;
             this.infoProducto.precioUnidad = this.datos[0].precio;
             this.infoProducto.cantidad = 1;
             this.infoProducto.precioTotal = this.datos[0].precio;
             this.infoProducto.fichaTecnica = this.datos[0].ficha;
-            if(this.sw = true){
+            if (this.sw = true) {
               this.listaProductos();
             }
           }
         }
       );
     }
+
   }
 
-  eliminarProducto(numProd: any){
+  eliminarProducto(numProd: any) {
     let i: any;
     let j: any;
-    j=0;
-    let productosTemp: any = [];
-    console.log("length inicial"+this.numero.length);
-    for(i = 0 ; i < this.numero.length;i++){
-      console.log("i: "+i);
-      if(i != numProd){
+    j = 0;
+    const productosTemp: any = [];
+    for (i = 0; i < this.numero.length; i++) {
+      if (i !== numProd) {
         productosTemp[j] = this.numero[i];
         j = j + 1;
-      }   
+      }
     }
     this.numero = productosTemp;
     this.precioTotal = 0;
-    for (let j = 0; j < this.numero.length; j++) {
+    for (j = 0; j < this.numero.length; j++) {
       this.precioTotal = this.numero[j].precioTotal + this.precioTotal;
-      console.log('entre');
     }
     this.valorTotalCot.emit(this.precioTotal);
     this.precioPagarCot.emit(this.precioTotal);
-    console.log("length final"+this.numero.length);
-    console.log(this.numero);
   }
 
-  
   lanzar() {
-    // Usamos el método emit
     this.valorTotalCot.emit('2000000');
-    console.log('se lan zo');
   }
 }
